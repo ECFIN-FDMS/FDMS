df25565f357b710a0f9e81121ea4b30c4bf66565
Marcos Bartolome <marcosbartolome@gmail.com>
2018-07-27 10:48:00 +0100

    Add Country and Variable groups

---
+++ config/country_groups.py
@@ -0,0 +1,26 @@
+country_group_names = {
+    'FCFTM': 'Forecast: Countries from transfer matrix',
+    'FCWVACP': 'Forecast: Countries with volumes at constant prices',
+    'EA': 'EA',
+    'FCWEMS1999': 'Forecast: Countries with EA membership since 1999',
+    'FCRIF': 'Forecast: Countries reporting in FTEs'
+}
+
+
+FCWVACP = ['EL', 'TR', 'RS', 'US', 'JP']
+
+
+FCRIF = ['ES', 'FR', 'IT', 'NL']
+
+
+FCWEMS1999 = ['BE', 'DE', 'IE', 'ES', 'FR', 'IT', 'LU', 'NL', 'AT', 'PT', 'FI']
+
+
+EA = ['BE', 'DE', 'EE', 'IE', 'EL', 'ES', 'FR', 'IT', 'CY', 'LV', 'LT', 'LU',
+      'MT', 'NL', 'AT', 'PT', 'SI', 'SK', 'FI']
+
+
+FCFTM = ['BE', 'BG', 'CZ', 'DK', 'DE', 'EE', 'IE', 'EL', 'ES', 'FR', 'HR',
+         'IT', 'CY', 'LV', 'LT', 'LU', 'HU', 'MT', 'NL', 'AT', 'PL', 'PT',
+         'RO', 'SI', 'SK', 'SE', 'FI', 'UK', 'TR', 'MK', 'ME', 'IS', 'RS',
+         'AL', 'US', 'JP', 'CH', 'NO', 'RU', 'DAE']
