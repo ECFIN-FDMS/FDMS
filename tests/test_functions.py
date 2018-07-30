98cedeec560c5a8933a2cb412edce32ffb361bf6
Marcos Bartolome <marcosbartolome@gmail.com>
2018-07-30 15:38:38 +0100

    Added tests and requirements.txt

---
+++ tests/test_functions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import pandas as pd
+from helpers.functions import Calculator
+
+
+class TestSplice(unittest.TestCase):
+    """Tests for 'Splice' function."""
+
+    def setUp(self):
+        self.data = pd.read_excel('tests/sample_data.xlsx', index_col=[0, 2])
+
+    def test_butt_splice(self):
+        series1 = self.data.loc[('BE', 'UTVTBP.1.0.0.0')].filter(regex='\d{4}')
+        self.assertEqual(series1.first_valid_index(), 1995)
+        self.assertEqual(series1.last_valid_index(), 2017)
+        self.assertEqual(2, 2)
+
