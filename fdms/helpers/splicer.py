c9233d529d79212130af58f1f90649e34f92c0c4
Marcos Bartolome <marcosbartolome@gmail.com>
2018-08-01 09:44:05 +0100

    #7 - Add buttsplice

--- fdms/helpers/splicer.py
+++ fdms/helpers/splicer.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 
 
 logger = logging.getLogger(__name__)
@@ -15,11 +16,10 @@ def get_series(dataframe, country_ameco, variable_code):
     return series
 
 
-class Calculator:
+class Splicer:
     '''Provides methods to perform required calculations with pandas DataFrames'''
 
-    def splice(self, base_series, splice_series, method, kind='both', overlap=False, replace_empty_base_series=True,
-               period=None):
+    def butt_splice(self, base_series, splice_series, kind='both', replace_empty_base_series=True, period=None):
         '''
         Splicing extends the ends of the base series with data values from another series. Splicing does not fill gaps
          within the base series.
@@ -29,55 +29,54 @@ class Calculator:
             -  Both directions, from both ends of the base series
         A common usage of splicing is to extend the base series with series from other sources. One example is to
          extend series in the working database with historical data from the archive database.
-        The following four options are available:
-            -  Butt: splicing the actual values
-            -  Level: splicing the period-over-period difference
-            -  Ratio: splicing the period-over-period ratio
-            -  YoY: splicing the same-period-over-year-ago ratio, not applicable for daily data
-        Percent splice is available as a separate function
+        BUTTSPLICE extends the base series by taking the values directly from the splice series.
 
         :param base_series: Required. Specify a time series or an expression to be used as a base for splicing.
         :type base_series: pandas.core.frame.DataFrame
         :param splice_series: Required. Specify a time series or an expression where values are to be used for splicing
          purpose.
         :type splice_series: pandas.core.frame.DataFrame
-        :param str method: Required. Specify one of the four methods:
-            -  Butt
-            -  Level
-            -  Ratio
-            -  YoY
-        :param str kind: Optional. "forward", "Backward" or "both". Default = both.
-        :param bool overlap: TODO: Check if we need this one
+        :param str kind: Optional. "forward", "Backward" or "both". Default = "both".
         :param bool replace_empty_base_series: TODO: Check if we need this one
         :param period: TODO: Check if we need this one
         :return:
         :rtype: pandas.core.frame.DataFrame
         '''
         # We assume both DataFrames have the same frequency for now
+
         result = base_series.copy()
+        name = result.name
         if kind == 'forward' or kind == 'both':
-            base_end = base_series.last_valid_index()
+            base_last_valid = base_series.last_valid_index()
+            base_last_index = base_series.index[-1]
             splice_end = splice_series.last_valid_index()
-            if base_end > splice_end:
-                logger.warning('Failed to splice {}, country {}, Splice series ends before base series').format(
+            if base_last_valid > splice_end:
+                logger.warning('Failed to splice {} forward, country {}, Splice series ends before base series').format(
                     base_series.name[0], base_series.name[1])
             else:
-                base_end_loc = base_series.index.get_loc(base_end)
-                forward_splice_start_loc = splice_series.index.get_loc(base_end_loc) + 1
+                base_last_valid_loc = base_series.index.get_loc(base_last_valid)
+                forward_splice_start_loc = splice_series.index.get_loc(base_last_valid) + 1
                 splice_end_loc = splice_series.index.get_loc(splice_end)
-                if method == 'butt':
-                    result.iloc[base_end_loc + 1:] = splice_series.iloc[forward_splice_start_loc:splice_end_loc]
-
+                result.iloc[base_last_valid_loc + 1:] = splice_series.iloc[forward_splice_start_loc:splice_end_loc]
+                if splice_series.index[-1] > base_last_index:
+                    splice_overflow_start_loc = splice_series.index.get_loc(base_last_index + 1)
+                    result = result.append(splice_series.iloc[splice_overflow_start_loc:])
 
         if kind == 'backward' or kind == 'both':
-            base_start = base_series.first_valid_index()
+            base_first_valid = base_series.first_valid_index()
+            base_first_index = base_series.index[0]
             splice_start = splice_series.first_valid_index()
-            if base_start < splice_start:
+            if base_first_valid < splice_start:
                 logger.warning('Failed to splice {}, country {}, Splice series starts after base series').format(
                     base_series.name[0], base_series.name[1])
             else:
-                base_start_loc = base_series.index.get_loc(base_start)
-                backward_splice_start_loc = splice_series.index.get_loc(base_start_loc) + 1
-                splice_end_loc = splice_series.index.get_loc(base_start_loc - 1)
-                if method == 'butt':
-                    result.iloc[:base_start_loc - 1] = splice_series.iloc[backward_splice_start_loc:splice_end_loc]
+                base_first_valid_loc = base_series.index.get_loc(base_first_valid)
+                backward_splice_start_loc = splice_series.index.get_loc(base_first_index)
+                splice_end_loc = splice_series.index.get_loc(base_first_valid - 1)
+                result.iloc[:base_first_valid_loc - 1] = splice_series.iloc[backward_splice_start_loc:splice_end_loc]
+                if splice_series.index[0] < base_first_index:
+                    splice_overflow_end = splice_series.index.get_loc(base_first_index)
+                    result = pd.concat([splice_series.iloc[:splice_overflow_end], result])
+                    result.name = name
+
+        return result
--- fdms/tests/test_functions.py
+++ fdms/tests/test_functions.py
@@ -4,16 +4,40 @@
 import unittest
 import pandas as pd
 
+from fdms.helpers.splicer import Splicer
+
 
 class TestSplice(unittest.TestCase):
-    '''Tests for "Splice" function.'''
+    '''Tests for "Splice" functions.'''
 
     def setUp(self):
-        self.data = pd.read_excel('fdms/tests/sample_data.xlsx', index_col=[0, 2])
+        self.base_dataframe = pd.read_excel('fdms/tests/sample_data.xlsx', sheet_name='base_series1', index_col=[0, 2])
+        self.splice_dataframe = pd.read_excel('fdms/tests/sample_data.xlsx', sheet_name='test_splice1', index_col=[0, 1])
 
     def test_butt_splice(self):
-        series1 = self.data.loc[('BE', 'UTVTBP.1.0.0.0')].filter(regex='\d{4}')
-        self.assertEqual(series1.first_valid_index(), 1995)
-        self.assertEqual(series1.last_valid_index(), 2017)
-        self.assertEqual(2, 2)
-
+        base_series = self.base_dataframe.loc[('BE', 'UTVTBP')].filter(regex='\d{4}')
+        splice_series = self.splice_dataframe.loc[('BE', 'UTVTBP.1.0.0.0')].filter(regex='\d{4}')
+        self.assertEqual(base_series.first_valid_index(), 2000)
+        self.assertEqual(base_series.last_valid_index(), 2013)
+        self.assertEqual(base_series.index[0], 1996)
+        self.assertEqual(base_series.index[-1], 2020)
+        self.assertEqual(splice_series.first_valid_index(), 1995)
+        self.assertEqual(splice_series.last_valid_index(), 2023)
+        self.assertEqual(splice_series.index[0], 1960)
+        self.assertEqual(splice_series.index[-1], 2025)
+        splicer = Splicer()
+        result_backward = splicer.butt_splice(base_series, splice_series, kind='backward')
+        self.assertEqual(result_backward.first_valid_index(), 1995)
+        self.assertEqual(result_backward.last_valid_index(), 2013)
+        self.assertEqual(result_backward.index[0], 1960)
+        self.assertEqual(result_backward.index[-1], 2020)
+        result_forward = splicer.butt_splice(base_series, splice_series, kind='forward')
+        self.assertEqual(result_forward.first_valid_index(), 2000)
+        self.assertEqual(result_forward.last_valid_index(), 2023)
+        self.assertEqual(result_forward.index[0], 1996)
+        self.assertEqual(result_forward.index[-1], 2025)
+        result_both = splicer.butt_splice(base_series, splice_series, kind='both')
+        self.assertEqual(result_both.first_valid_index(), 1995)
+        self.assertEqual(result_both.last_valid_index(), 2023)
+        self.assertEqual(result_both.index[0], 1960)
+        self.assertEqual(result_both.index[-1], 2025)
