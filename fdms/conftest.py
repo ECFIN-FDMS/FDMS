498f88bc34b756499e26ee4296abb67f750402a3
Marcos Bartolome <marcosbartolome@gmail.com>
2018-12-12 16:35:11 +0100

    FDMSSTAR-53 - Parametrize tests

--- fdms/conftest.py
+++ fdms/conftest.py
@@ -7,4 +7,5 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='class')
 def country(request):
+    import code;code.interact(local=locals())
     request.cls.country = request.config.getoption("--country")
