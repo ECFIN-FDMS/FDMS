07364ab6fd2e6fc1b107c559bace3e35f3905a4c
Umberto Passanisi <umbertopassanisi@gmail.com>
2019-01-07 14:31:08 +0100

    added tests for steps 15 and 16

--- fdms/tests/test_country_calculations.py
+++ fdms/tests/test_country_calculations.py
@@ -201,6 +201,14 @@ class TestCountryCalculations(unittest.TestCase):
         missing_vars = [v for v in variables if v not in list(result_14.loc[self.country].index)]
         self.assertFalse(missing_vars)
 
+        # STEP 15
+        step_15 = PrivateSector(scales=self.scales)
+        result_15 = step_15.perform_computation(self.result_1, result_12, result_13, result_14, self.ameco_df)
+
+        # STEP 16
+        step_16 = ExternalSector(scales=self.scales)
+        result_16 = step_16.perform_computation(self.result_1, result_3, self.ameco_df)
+
         # TODO: Fix all scales
         result = pd.concat([self.result_1, result_2, result_3, result_4, result_5, result_6, result_7, result_8,
                             result_9, result_10, result_11, result_12, result_13, result_14], sort=True)
